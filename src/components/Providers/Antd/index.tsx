f488a02756e379394fb6b4056d14f7a1e0088905
rodertd2000 <zdabaghov@mail.ru>
2024-04-05 15:46:20 +0300

    config provider

--- src/components/Providers/Antd/index.tsx
+++ src/components/Providers/Antd/index.tsx
@@ -9,12 +9,27 @@ export const AntdProvider: FC<Props> = ({ children }) => {
   return (
     <ConfigProvider
       theme={{
+        token: {
+          fontFamily: "Roboto",
+        },
         components: {
           Card: {
             borderRadiusLG: 12,
           },
           List: {
             itemPadding: "6px 0",
+          },
+          Input: {
+            paddingInline: 12,
+            paddingBlock: 12,
+            fontSize: 16,
+            borderRadius: 8,
+          },
+          DatePicker: {
+            paddingInline: 12,
+            paddingBlock: 12,
+            fontSize: 14,
+            borderRadius: 8,
           },
         },
       }}
